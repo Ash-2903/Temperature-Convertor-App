ee4839e2f1c09331f7f7487baa131c2d78f49036
ashritha herle <ashrithaherle2903@gmail.com>
2023-10-28 13:09:24 +0530

    add layout and components

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.temperatureconvertor"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.temperatureconvertor"
@@ -36,7 +36,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.11.0-beta01")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//    implementation ("androidx.navigation:navigation-fragment:$nav_version")
+//    implementation ("androidx.navigation:navigation-ui:$nav_version")
 }
